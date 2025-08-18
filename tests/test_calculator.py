b0263c667b1f2653b50eed793cdefcb5d6297cc8
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-18 14:37:07 -0400

    chore: add package init file

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,8 +1,4 @@
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-from calculator import add, subtract  # noqa: E402
+from verbose_fishstick.calculator import add, subtract
 
 
 def test_addition():
